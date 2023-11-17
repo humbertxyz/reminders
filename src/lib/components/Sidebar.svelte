d5066f6f70d25192962ba46a3a03962851f32026
humbertxyz <149017485+humbertxyz@users.noreply.github.com>
2023-11-17 19:20:16 +0000

    Add sidebar mockup

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,32 @@
+
+<script>
+  export let user = '';
+</script>
+
+<div class="sidebar">
+  <div class="sidebar-section">
+    {#if user}
+      <p>Logged in as {user}</p>
+      <button>Logout</button>
+    {:else}
+      <button>Login</button>
+    {/if}
+  </div>
+  
+  <div class="sidebar-section">
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/accounts">Accounts</a></li>
+      <li><a href="/subscriptions">Subscriptions</a></li>
+    </ul>
+  </div>
+  
+  <div class="sidebar-section">
+    <h3>Settings</h3>
+    <ul>
+      <li><a href="/settings/profile">Profile</a></li>
+      <li><a href="/settings/preferences">Preferences</a></li>
+      <li><a href="/settings/security">Security</a></li>
+    </ul>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import "$packages/scss/styles.scss";
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	let user = 'John Doe';
 </script>
 
+<Sidebar user={user} />
 <main>
 	<slot></slot>
 </main>
