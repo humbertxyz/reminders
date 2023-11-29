1fdf2b4585de59ee9e5fc269e531036c816b049a
humbertxyz <149017485+humbertxyz@users.noreply.github.com>
2023-11-29 15:09:56 +0000

    Layout them variable fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,19 +30,19 @@
 		--black: #000000;
 
 		--primary-light: #4B9CD3;
-		--primary-main: #1A73E8;
+		--primary: #1A73E8;
 		--primary-dark: #103C60;
 
 		--success-light: #6DDF6D;
-		--success-main: #28A745;
+		--success: #28A745;
 		--success-dark: #165B33;
 
 		--warning-light: #FFC107;
-		--warning-main: #FFA000;
+		--warning: #FFA000;
 		--warning-dark: #C67C00;
 
 		--danger-light: #E57373;
-		--danger-main: #DC3545;
+		--danger: #DC3545;
 		--danger-dark: #A71D2A;
 
 		--ff: Roboto, system-ui, Helvetica, Verdana, Arial, "Times New Roman";
