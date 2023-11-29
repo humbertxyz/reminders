4a37b9cd5b8349c8f89e36b1fd222840d409c930
humbertxyz <149017485+humbertxyz@users.noreply.github.com>
2023-11-29 15:11:37 +0000

    Replace supabase UI to OTP link

---
+++ src/lib/components/LoginMagicLink.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { ButtonProps } from "$lib/typing/ui";
+	import Button from "$lib/ui/Button.svelte";
+
+
+	let buttonProps: ButtonProps = {
+		text: "Send magic link",
+		onClick: () => {
+			console.log("clicked");
+		},
+		disabled: false,
+		type: "button",
+		flavor: "danger",
+		is_block: false,
+	};
+</script>
+
+<div class="magic-link">
+	<h1>Log in</h1>
+	<p>
+		If you have an account, we'll send you a magic link to log in.
+	</p>
+	<Button props={buttonProps} />
+</div>
+
+<style>
+	.magic-link {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+	}
+	.magic-link h1 {
+		font-size: 2rem;
+	}
+	.magic-link p {
+		font-size: 1.2rem;
+	}
+</style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { Auth } from '@supabase/auth-ui-svelte';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
-
+	import LoginMagicLink from '$lib/components/LoginMagicLink.svelte';
 	export let data;
 </script>
 
@@ -9,14 +7,4 @@
 	<title>User Management</title>
 </svelte:head>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/auth/callback`}
-			showLinks={true}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
-		/>
-	</div>
-</div>
+<LoginMagicLink />
